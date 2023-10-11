051bde7b75ab35692232bb54f499db4d7234a50c
Valentina <valenpinzonn@gmail.com>
2023-10-10 21:58:15 -0500

    chws with master

---
+++ react_components/dashboard/src/pages/ChwsPromoter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../components/shared/buttons/styles/Button.css'; 
+import '../components/shared/buttons/styles/ButtonSkills.css'; 
+import {  } from "../styles/Profile.css";
+import Header from '../components/core/Header';
+import {} from '../styles/Header.css'
+
+const ChwsPromoter = () => {
+
+  return (
+    <div>
+      <div>
+      <Header></Header>
+      </div>
+      <div>
+      <h1 className='title-h1'style={{ paddingTop: '20px', fontWeight: 'bold'}}>CHW/Promotor Philosophy</h1>
+      </div>
+
+    </div>
+  );
+};
+
+
+export default ChwsPromoter;
